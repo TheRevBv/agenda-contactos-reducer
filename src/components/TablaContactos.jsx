79a33d0edada18b6e7f195d4c8895343f597efbe
Joshua Morin <73865022+TheRevBv@users.noreply.github.com>
2023-11-02 17:17:29 -0600

    edad

--- src/components/TablaContactos.jsx
+++ src/components/TablaContactos.jsx
@@ -2,6 +2,12 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
+const getEdad = (fechaNacimiento) => {
+  const fecha = new Date();
+  const edad = fecha.getFullYear() - fechaNacimiento;
+  return edad;
+};
+
 const TablaContactos = ({ contactos = [], dispatch }) => {
   // Definimos el método handleDelete:
   const handleDelete = (id) => {
@@ -15,51 +21,37 @@ const TablaContactos = ({ contactos = [], dispatch }) => {
   };
 
   return (
-    <table className="table col-md-6">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Avatar</th>
-          <th>Nombre</th>
-          <th>Número</th>
-          <th>Sexo</th>
-          <th>Edad</th>
-          <th>Acciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        {contactos.map((contacto) => {
-          const finalId = contacto.id.split("-");
-
-          return (
+    <div>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Fecha de Nacimiento</th>
+            <th>Edad</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contactos.map((contacto) => (
             <tr key={contacto.id}>
-              <td>{finalId[0]}</td>
-              <td>
-                <img
-                  src={contacto.avatar}
-                  alt={contacto.nombre}
-                  width={50}
-                  height={50}
-                  className="rounded-circle"
-                />
-              </td>
               <td>{contacto.nombre}</td>
-              <td>{contacto.numero}</td>
-              <td>{contacto.sexo}</td>
-              <td>{2023 - contacto.fechaNacimiento}</td>
+              <td>{contacto.apellido}</td>
+              <td>{contacto.fechaNacimiento}</td>
+              <td>{getEdad(contacto.fechaNacimiento)}</td>
               <td>
                 <button
                   className="btn btn-danger"
                   onClick={() => handleDelete(contacto.id)}
                 >
-                  Eliminar
+                  Borrar
                 </button>
               </td>
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
